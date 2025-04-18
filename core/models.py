200c69430d1006af81110a7b162508f25ba812f0
Satyam Yadav <167583328+SimpleCyber@users.noreply.github.com>
2025-04-18 21:48:42 +0530

    Update models.py

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class QueryLog(models.Model):
     query = models.TextField()
-    tone = models.CharField(max_length=100)
-    intent = models.CharField(max_length=100)
+    tone = models.CharField(max_length=1000)
+    intent = models.CharField(max_length=1000)
     suggested_actions = models.JSONField()
     timestamp = models.DateTimeField(auto_now_add=True)
